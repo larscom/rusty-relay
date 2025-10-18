285f6149b533756895a1a074ac5485ad4bfab68c
larscom <larskniep@gmail.com>
2025-10-18 20:12:52 +0200

    add axum server

--- crates/relay-server/src/main.rs
+++ crates/relay-server/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, response::IntoResponse, routing};
+
+async fn handler() -> impl IntoResponse {
+    "Hello!"
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt::init();
+
+    // let state = Arc::new(AppState::new());
+    let app = Router::new().route("/hello", routing::get(handler));
+    // .route("/webhook/:id", post(receive_webhook))
+    // .route("/connect/:id", axum::routing::get(ws::handle_ws))
+    // .with_state(state.clone());
+
+    let addr = "0.0.0.0:8080";
+    tracing::info!("🚀 relay server running on {}", addr);
+    let listener = tokio::net::TcpListener::bind(addr).await?;
+    Ok(axum::serve(listener, app).await?)
 }
