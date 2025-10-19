f5aba68f562f6095368e6ec5020f2a86d5e5ed6d
larscom <larskniep@gmail.com>
2025-10-19 21:57:01 +0200

    working client

--- crates/relay-client/src/main.rs
+++ crates/relay-client/src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use futures_util::StreamExt;
+use reqwest::Client;
+use tokio_tungstenite::{connect_async, tungstenite::Message};
+
+#[derive(Parser, Debug)]
+#[command(name = "rusty-relay")]
+struct Args {
+    #[arg(long)]
+    server: String,
+    #[arg(long)]
+    id: String,
+    #[arg(long)]
+    target: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt::init();
+    let args = Args::parse();
+
+    tracing::info!("🔗 connecting to relay server {}", args.server);
+    connect(args.server, args.id, args.target).await?;
+    Ok(())
+}
+
+pub async fn connect(
+    server: String,
+    id: String,
+    target: String,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let url = format!("{server}/ws/{id}");
+    dbg!(&url);
+    let (mut ws_stream, _) = connect_async(url).await?;
+
+    while let Some(msg) = ws_stream.next().await {
+        if let Ok(Message::Text(payload)) = msg {
+            forward(&target, &payload).await?;
+        }
+    }
+
+    Ok(())
+}
+
+pub async fn forward(target: &str, payload: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::builder().use_rustls_tls().build()?;
+    let res = client.post(target).body(payload.to_string()).send().await?;
+    tracing::info!("➡️  forwarded webhook, got {}", res.status());
+    Ok(())
 }
